bc849112d8c3d46f3dee04e2d49d398f2f910c9a
Harp Sandhu <happacode@protonmail.com>
2021-05-17 17:37:12 -0700

    Fix getInterview errors on running Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -27,7 +27,8 @@ export default function Application(props) {
       .then((all) => {
         setState(prev => ({...prev, 
           days: all[0].data, 
-          appointments: all[1].data
+          appointments: all[1].data,
+          interviewers: all[2].data
         }))
       })  
   }, [])
