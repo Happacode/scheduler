9263da2dcf251b78e486b1a06b57e7d29e390fb0
Harp Sandhu <happacode@protonmail.com>
2021-05-19 14:59:40 -0700

    Add custom hook useApplicationData by separation of concerns design

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,62 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import useApplicationData from "hooks/useApplicationData";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 
 export default function Application(props) {
   
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    setState({
-      ...state,
-      appointments
-    });
-    return axios.put(`/api/appointments/${id}`, {interview})
-      .then(res => console.log(res));
-  }
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments
-    };
-    appointment[id].interview = null;
-    return axios.delete(`/api/appointments/${id}`)
-    .then(res => console.log(res))
-  }
-
-  const setDay = day => setState({ ...state, day });
-  
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'), 
-      axios.get('/api/appointments'), 
-      axios.get('/api/interviewers')])
-      .then((all) => {
-        setState(prev => ({...prev, 
-          days: all[0].data, 
-          appointments: all[1].data,
-          interviewers: all[2].data
-        }))
-      })  
-  }, [])
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
   
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
